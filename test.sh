b5b10c5e3c888ca388226b57ad0b9e34cac7d47d
DaoThanhTung <tung.daothanhtung@gmail.com>
2026-02-21 21:27:24 +0700

    add Cursor allowlists

--- test.sh
+++ test.sh
@@ -4,15 +4,21 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # run this script in project root directory before commit
 
 # optional format the whole repo
+echo "running goimports ..."
 goimports -w .
+echo "end goimports"
 
 # Vet examines Go source code and reports suspicious constructs
+echo "running go vet ..."
 go vet ./...
+echo "end go vet"
 
 # Run all unittests, include database tests, which require Docker compose to be running.
-go clean -testcache
-# go test -v ./...
-go test ./...
+# go clean -testcache
+echo "running go test ..."
+go test -v ./...
+# go test ./...
+echo "end go test"
 
 echo "========================================"
 echo "All tests passed!"
